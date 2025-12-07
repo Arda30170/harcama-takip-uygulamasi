3d52af6db6f94917e65a494d943f1371b0335cc3
arda30170 <ardakoca957@gmail.com>
2025-12-08 00:19:15 +0300

    create console menu

---
+++ main.py
@@ -0,0 +1,13 @@
+harcamalar = []
+
+while True:
+    print("** \n Harcama Takip Uygulaması **")
+    print("1-Harcama Ekle")
+    print("2-Harcamalarımı Görüntüle")
+    print("3-Harcamalatı Kaydet")
+    print("4-Kategoriye Gore Harcama Goster")
+    print("Cıkıs Yap")
+
+    islem = input(" \n Yapmak istediginiz islemi secin: ")
+
+
